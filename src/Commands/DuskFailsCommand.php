5ef1bce6b207750be9f9102e1670a2f8d5922e76
sampoyigi <sampoyigi@users.noreply.github.com>
2024-08-11 10:59:00 +0000

    PHP Linting (Pint) - Fix styling

--- src/Commands/DuskFailsCommand.php
+++ src/Commands/DuskFailsCommand.php
@@ -2,6 +2,4 @@
 
 namespace Igniter\Dusk\Commands;
 
-class DuskFailsCommand extends \Laravel\Dusk\Console\DuskFailsCommand
-{
-}
+class DuskFailsCommand extends \Laravel\Dusk\Console\DuskFailsCommand {}
